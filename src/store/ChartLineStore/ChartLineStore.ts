27ec6dbf1626d1d79cfa513af68ac54b3a79c8bf
Julia <yuliabobrovich@yandex.ru>
2022-09-06 14:04:56 +0300

    Created dynamic line chart

--- src/store/ChartLineStore/ChartLineStore.ts
+++ src/store/ChartLineStore/ChartLineStore.ts
@@ -1,4 +1,4 @@
-import styles from "@pages/Coin/components/Chart/styles.module.scss";
+import styles from "@pages/Coin/components/ChartLine/styles.module.scss";
 import rootStore from "@store/RootStore/instance";
 import { log } from "@utils/log";
 import { ILocalStore } from "@utils/useLocalStore";
@@ -8,12 +8,12 @@ import {
   observable,
   action,
   computed,
+  reaction,
   runInAction,
+  IReactionDisposer,
 } from "mobx";
-import { asObservableObject } from "mobx/dist/internal";
 
 export enum Periods {
-  H1 = "1 H",
   H24 = "24 H",
   W1 = "1 W",
   M1 = "1 M",
@@ -105,31 +105,128 @@ export default class ChartStore implements ILocalStore {
   }
 
   pricesRequest = async () => {
+    let days: number = 1;
+    if (this._clickedPeriod == Periods.H24) {
+      days = 1;
+    } else if (this._clickedPeriod == Periods.W1) {
+      days = 7;
+    } else if (this._clickedPeriod == Periods.M1) {
+      days = 30;
+    } else if (this._clickedPeriod == Periods.M6) {
+      days = 183;
+    } else if (this._clickedPeriod == Periods.Y1) {
+      days = 365;
+    }
+
     if (this._id != undefined) {
       const result = await axios({
         method: "get",
-        url: `https://api.coingecko.com/api/v3/coins/${this._id}/market_chart?vs_currency=${rootStore.currency.currency.key}&days=1`,
+        url: `https://api.coingecko.com/api/v3/coins/${this._id}/market_chart?vs_currency=${rootStore.currency.currency.key}&days=${days}`,
       });
 
       runInAction(() => {
-        result.data.prices
-          .filter((price: number[]) => {
-            return (
-              new Date(price[0]).getMinutes() == 0 ||
-              new Date(price[0]).getMinutes() == 1
-            );
-          })
-          .map((price: number[]) => {
-            this._dates.push(`${new Date(price[0]).getHours()}:00`);
-            this._prices.push(price[1]);
-            // log("date:", new Date(price[0]).getHours(), "hours");
-            // log("price:", `${rootStore.currency.currency.symbol} ${price[1]}`);
-          });
+        const weekday = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        const months = [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "June",
+          "July",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec",
+        ];
+
+        if (this._clickedPeriod == Periods.H24) {
+          result.data.prices
+            .filter((price: number[]) => {
+              return (
+                new Date(price[0]).getMinutes() == 0 ||
+                new Date(price[0]).getMinutes() == 1
+              );
+            })
+            .map((price: number[]) => {
+              this._dates.push(`${new Date(price[0]).getHours()}:00`);
+              this._prices.push(price[1]);
+            });
+        } else if (this._clickedPeriod == Periods.W1) {
+          result.data.prices
+            .filter((price: number[]) => {
+              return new Date(price[0]).getHours() == 0;
+            })
+            .map((price: number[]) => {
+              const day = weekday[new Date(price[0]).getDay()];
+
+              this._dates.push(day);
+              this._prices.push(price[1]);
+            });
+        } else if (this._clickedPeriod == Periods.M1) {
+          result.data.prices
+            .filter((price: number[]) => {
+              return new Date(price[0]).getHours() == 0;
+            })
+            .map((price: number[]) => {
+              const month = months[new Date(price[0]).getMonth()];
+
+              this._dates.push(`${new Date(price[0]).getDate()} ${month}`);
+              this._prices.push(price[1]);
+            });
+        } else if (this._clickedPeriod == Periods.M6) {
+          result.data.prices
+            .filter((price: number[]) => {
+              return new Date(price[0]).getDate() == 1;
+            })
+            .map((price: number[]) => {
+              const month = months[new Date(price[0]).getMonth()];
+              this._dates.push(month);
+              this._prices.push(price[1]);
+            });
+        } else if (this._clickedPeriod == Periods.Y1) {
+          const months = [
+            "Jan",
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "June",
+            "July",
+            "Aug",
+            "Sep",
+            "Oct",
+            "Nov",
+            "Dec",
+          ];
+
+          result.data.prices
+            .filter((price: number[]) => {
+              return new Date(price[0]).getDate() == 1;
+            })
+            .map((price: number[]) => {
+              const month = months[new Date(price[0]).getMonth()];
+              this._dates.push(month);
+              this._prices.push(price[1]);
+            });
+        }
       });
     } else {
       log("id not found");
     }
   };
 
-  destroy(): void {}
+  destroy(): void {
+    this._periodChangeHandler();
+  }
+
+  readonly _periodChangeHandler: IReactionDisposer = reaction(
+    () => this._clickedPeriod,
+    () => {
+      this._dates.length = 0;
+      this._prices.length = 0;
+      this.pricesRequest();
+    }
+  );
 }
